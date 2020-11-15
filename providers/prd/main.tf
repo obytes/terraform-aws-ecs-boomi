1be5771156fd541d3d76c58e7c0d7142b0ceca5a
Ahmed AL Haffar <alhaffar.ahmed@gmail.com>
2020-11-15 13:27:44 +0300

    Migrating form S3 backend to local

--- providers/prd/main.tf
+++ providers/prd/main.tf
@@ -1,10 +1,6 @@
 terraform {
-  backend "s3" {
-    bucket                  = "terraform-boomi"
-    key                     = "aws/us-east1/prd/terraform.tfstate"
-    region                  = "us-east-1"
-    shared_credentials_file = "~/.aws/credentials"
-    profile                 = "ipolls"
+  backend "local" {
+    path = "../.terraform/terraform_local.tfstate"
   }
 
   required_version = "= 0.13.0"
